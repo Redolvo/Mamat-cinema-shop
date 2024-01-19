bd2f0c089f2fabac77cb3cc55c965f561ae26029
ClaysHere <muhammadfikri4402@gmail.com>
2024-01-19 12:05:09 +0700

    add colors

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                blacks: {
+                    500: "#000814",
+                },
+                greys: {
+                    500: "#E7EAEF",
+                },
+                blues: {
+                    400: "#0F6BC1",
+                    500: "#003566",
+                    600: "#001D3D",
+                },
+                yellows: {
+                    400: "#FBDD46",
+                    500: "#F5CC00",
+                    600: "#CCA000",
+                },
+            },
+        },
     },
     plugins: [],
 };
