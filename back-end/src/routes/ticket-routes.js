b47cd1113fc75ad670de5052032077609fcfd3f7
leonardo <leonardoaahh@gmail.com>
2024-01-21 18:26:22 +0700

    get user ticket api

--- back-end/src/routes/ticket-routes.js
+++ back-end/src/routes/ticket-routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {submitTicket} = require('../controllers/Ticket');
+const {submitTicket, getUserTicket} = require('../controllers/Ticket');
 
+router.get('/get-user-ticket', getUserTicket)
 router.post('/submit-ticket', submitTicket)
 
 module.exports = router;
